f4b1126f3334e95508104d792b9521cde68c29d5
Liemarts <liemarts8@gmail.com>
2024-02-05 21:02:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [myAdvice, setMyAdvice] = useState ("");
+
+  useEffect (()=> {
+    getAdvice();
+  }, [])
+
+  const getAdvice = async ()=> {
+    const response = await fetch ('http://www.boredapi.com/api/activity/');
+    const data = await response.json();
+    setMyAdvice (data.activity);
+  }
+
+  return (
+    <div className="App">
+      <div className='container'>
+        <h1>There's no time to be bored!</h1>
+      </div>
+
+      <div className='container'>
+        <h2>{myAdvice}</h2>
+      </div>
+
+      <div className='container'>
+        <button onClick={getAdvice}>Just try!</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
